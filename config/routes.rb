513598c7da45ac549c41274b3b8687458e1bb644
Oscar <oscar@customquests.com>
2012-03-15 23:24:47 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
 
-  get "pages/contact"
+  root :to => 'pages#home'
 
-  get "pages/about"
+  root_path => '/'
+  root_url  => 'http://localhost:3000/'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
