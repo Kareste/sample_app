5d263993cf9aa807db87269bbe440385316e4d82
Oscar <oscar@customquests.com>
2012-03-16 16:04:33 -0400

    Finished layout and routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+describe "LayoutLinks" do
+	it "should have the right links on the Layout" do
+		visit root_path
+		click_link "About"
+		response.should have_selector('title', :content => "About")
+		click_link "Help"
+		response.should have_selector('title', :content => "Help")
+		click_link "Contact"
+		response.should have_selector('title', :content => "Contact")
+		click_link "Home"
+		response.should have_selector('title', :content => "Home")
+		click_link "Sign up"
+		response.should have_selector('title', :content => "Sign up")
+	end
+	
+end
